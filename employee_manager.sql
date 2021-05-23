6ca69b3fced20a454cd9c5e10420502a207efae5
Brett Treweek <bretttrew@gmail.com>
2021-05-23 20:48:10 +0800

    view all, quit and add department working

--- employee_manager.sql
+++ employee_manager.sql
@@ -1,19 +1,47 @@
 CREATE DATABASE employee_manager;
 
+set foreign_key_checks=0;
+
 USE employee_manager;
 
 CREATE TABLE Department (
-    id INT PRIMARY KEY ,
-    department_name VARCHAR(30)
-);
-CREATE TABLE Employees (
-    id INT PRIMARY KEY ,
-    department_name VARCHAR(30)
-);
-CREATE TABLE Roles  (
-    id INT PRIMARY KEY ,
-    department_name VARCHAR(30)
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30) NOT NULL 
 );
 
-INSERT INTO Department (id, department_name)
-VALUES (2, 'sales');
+CREATE TABLE Employees (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    CONSTRAINT FK_EmployeeRole FOREIGN KEY(role_id)
+    REFERENCES Roles (id),
+    CONSTRAINT FK_EmployeeManager FOREIGN KEY(manager_id)
+    REFERENCES Employees(id)
+);
+
+CREATE TABLE Roles  (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL, 
+    department_id INT,
+    PRIMARY KEY (id),
+    CONSTRAINT FK_RoleDepartment 
+    FOREIGN KEY(department_id)
+    REFERENCES Department(id)
+    );
+
+INSERT INTO Department ( department_name)
+VALUES ('sales');
+INSERT INTO Department (department_name)
+VALUES ('marketing');
+INSERT INTO Employees (first_name, last_name, role_id, manager_id)
+VALUES ('Brett', 'Treweek', 123, 345);
+INSERT INTO Employees (first_name, last_name, role_id, manager_id)
+VALUES ('Meryl', 'Faitg', 123, 345);
+INSERT INTO Roles (title, salary, department_id)
+VALUES ('Bossy Boots', '50500', 1);
+INSERT INTO Roles (title, salary, department_id)
+VALUES ('Donkey', '600', 2);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+// const department = require("./department");
 const { promisify } = require("util");
 
 const connection = mysql.createConnection({
@@ -10,11 +11,9 @@ const connection = mysql.createConnection({
   database: "employee_manager",
 });
 
-// const query = promisify(connection.query).bind(connection);
+const query = promisify(connection.query).bind(connection);
 
-
-
-// const readDepartment =async () => {
+// const readDepartment = async () => {
 //     console.log('Selecting all products...\n');
 //     const res = await query('SELECT * FROM Department' );
 //     console.table(res);
@@ -22,55 +21,147 @@ const connection = mysql.createConnection({
 
 //   readDepartment();
 
-const menuList = async () => {
-  const answer = await inquirer.prompt([
-    {
-      type: "list",
-      name: "menuList",
-      message: "What would you like to do?",
-      choices: ["View", "Add", "Update"],
-    },
-  ]);
+console.log(
+  "========================== Welcome To Easy Employee Manager! ==========================\n"
+);
 
-  switch (answer.menuList) {
-    case "View":
-      view();
-      break;
-    case "Add":
-      add();
-      break;
-    case "Update":
-      update();
-      break;
-  }
+function menu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menuList",
+        message: "What would you like to do?",
+        choices: ["View", "Add", "Update", "Delete", "Quit"],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.menuList) {
+        case "View":
+          view();
+          break;
+        case "Add":
+          add();
+          break;
+        case "Update":
+          update();
+          break;
+        case "Delete":
+          remove();
+          break;
+        default:
+          quit();
+      }
+    });
+}
+
+function quit() {
+  connection.end();
+  console.log('============ Application Ended ==============')
+}
+
+const view = async () => {
+  const answer = await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "viewStuff",
+        message: "What would you like to view?",
+        choices: ["Department", "Employees", "Roles"],
+      },
+    ])
+    .then(async (answer) => {
+      const result = await query(`SELECT * FROM ${answer.viewStuff}`);
+      console.table(result);
+      menu();
+    });
 };
 
 const add = async () => {
-const answer = await inquirer.prompt([
-  {
-    type: "list",
-    name: "addStuff",
-    message: "what would you like to add?",
-    choices: ["department", "employee", "role"],
-  },
-]);
-switch (answer.addStuff) {
-  case "department":
-    createDepartment();
-    break;
-  case "employee":
-    createEmployees();
-    break;
-  default:
-    createRoles();
-}};
+  const answer = await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "addStuff",
+        message: "What would you like to add to?",
+        choices: ["Department", "Employees", "Roles"],
+      },
+    ])
+    .then(() => {
+      addDepartment()
+    });
+};
 
-menuList();
-// function view() {}
-// function update() {}
+const addDepartment = async () => {
+  const answer = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'departmentName',
+      message: 'What is the Department Name?',
+      validate: (answer) => {
+        if(answer === ''){
+          return 'Please enter a valid name';
+        }
+        return true;
+      }
+    }
+  ]).then(async (answer) => {
+    const result = await query(`INSERT INTO Department\(department_name\) VALUES \('${answer.departmentName}'\)`);
+    const res = await query(`SELECT * FROM Department` );
+      console.log(`\n====== Created new department ${answer.departmentName} ======\n`)
+      console.table(res);
+      menu();
+  })
+};
 
+const update = async () => {
+  const answer = await inquirer.prompt([
+    {
+      type: "list",
+      name: "updateStuff",
+      message: "What would you like to update?",
+      choices: ["Department", "Employees", "Roles"],
+    },
+  ]);
+  switch (answer.addStuff) {
+    case "Department":
+      updateDepartment();
+      break;
+    case "Employee":
+      updateEmployees();
+      break;
+    default:
+      updateRoles();
+  }
+};
+
+const remove = async () => {
+  const answer = await inquirer.prompt([
+    {
+      type: "list",
+      name: "deleteStuff",
+      message: "What would you like to delete?",
+      choices: ["Department", "Employees", "Roles"],
+    },
+  ]);
+  switch (answer.addStuff) {
+    case "Department":
+      removeDepartment();
+      break;
+    case "Employee":
+      removeEmployees();
+      break;
+    default:
+      removeRoles();
+  }
+};
+
+menu();
 
 connection.connect((err) => {
-    if(err) throw err;
-    // console.log(`MySql connected as id ${connection.threadId}`);
+  if (err) throw err;
+  // console.log(`MySql connected as id ${connection.threadId}`);
 });
+
+
+// module.exports = menu;
